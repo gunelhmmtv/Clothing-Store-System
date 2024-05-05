5b75b962d83abd6163660c67af43ffb3f49bd730
gunelhmmtv <gunell.hummetovaa@gmail.com>
2024-05-05 21:12:09 +0400

    ClothingStore

--- Vallahson/obj/Debug/net8.0-windows/Vallahson.AssemblyInfo.cs
+++ Vallahson/obj/Debug/net8.0-windows/Vallahson.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vallahson")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0f679ef95d1ca40a5f45d6735cc41b19b3e5256")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vallahson")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vallahson")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
